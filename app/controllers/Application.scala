0aa53da9d7ee1f92de7318ec86d44e5b5ffde32c
Carl Harroch <carl@novoda.com>
2012-06-12 13:13:02 +0100

    .

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,20 @@
 package controllers
 
 import play.api._
+import db.DB
 import play.api.mvc._
+import models.Task
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(Task.all()))
   }
-  
+
+  def postmark = Action(parse.json) {
+    request =>
+      Task.create(request.body.toString())
+      Ok
+  }
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "mel.to"
-    val appVersion      = "1.0"
+  val appName = "mel.to"
+  val appVersion = "1.0"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+  )
 
 }
